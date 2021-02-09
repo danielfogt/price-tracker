0b736a425811bfeda69ca951822a4fc864ed2978
daniel <daniel.j.fogt@gmail.com>
2021-02-09 08:57:14 +0100

    Configure pytest
    Configure pytest and django-pytest library in project to write future tests

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 app_name = "accounts"
 urlpatterns = [
-    path("", UserListView.as_view(), name="user-list"),
+    path("user-list/", UserListView.as_view(), name="user-list"),
     path("profile/", ProfileRetrieveUpdateView.as_view(), name="profile-update/"),
 ]
